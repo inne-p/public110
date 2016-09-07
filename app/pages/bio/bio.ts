cfa9e2a691dd88a96bb08c8dfda77360888e9399
Inne Prinusantari <ineprinusantari@gmail.com>
2016-09-07 08:48:51 +0700

    Add Bio pages
    For fill user bio

---
+++ app/pages/bio/bio.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the BioPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/bio/bio.html',
+})
+export class BioPage {
+
+  constructor(private navCtrl: NavController) {
+
+  }
+
+}
