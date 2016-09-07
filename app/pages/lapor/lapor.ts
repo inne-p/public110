39508930868ba302e4b9819cf94f5b2492f5221f
Inne Prinusantari <ineprinusantari@gmail.com>
2016-09-07 08:53:18 +0700

    Update lapor.ts

--- app/pages/lapor/lapor.ts
+++ app/pages/lapor/lapor.ts
@@ -1,19 +1,115 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, SqlStorage, Storage,  AlertController } from 'ionic-angular';
+import {Camera} from 'ionic-native';
+import { Geolocation } from 'ionic-native';
+import {SqliteService, DataLaporan} from '../../providers/sqlite-service/sqlite-service';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { LocationTracker } from '../../providers/location-tracker/location-tracker';
 
-/*
-  Generated class for the LaporPage page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
+  selector: 'lapor',
   templateUrl: 'build/pages/lapor/lapor.html',
+  providers: [SqliteService]
 })
+
 export class LaporPage {
+  selectedItem: any;
+  public showing= true;
+  private storage: Storage;
+  public base64Image: string;
+  laporan :any = {}
+  public url: string = "http://107.167.177.146/api-public110/api/";
 
-  constructor(private navCtrl: NavController) {
+  constructor(private nav: NavController, private http: Http, public sqliteService: SqliteService, public tracker: LocationTracker, private alertController:  AlertController) {
+	this.base64Image = "build/img/150x150.png";
 
+   }
+
+  saveIt() {
+
+	console.log("saveIt");
+
+    this.showing = false;
+	Geolocation.getCurrentPosition().then((position) => {
+	    this.laporan.koordinat = "{" + position.coords.latitude + "," + position.coords.longitude + "}";
+      this.base64Image = "150x150.png";
+		if (this.base64Image) {
+			this.laporan.foto = this.base64Image;
+		}
+
+		//this.sqliteService.saveLaporan(this.laporan);
+
+		let url= this.url + "public_reports?access_token=" + this.tracker.token;
+		let postBodyLogin: any = {
+		  "id" : 0,
+		  "foto": this.base64Image,
+		  "isilaporan": this.laporan.isiLaporan,
+		  "jenis": this.laporan.jenis,
+		  "email": this.tracker.nik,
+		  "tgl": new Date().toJSON(),
+		  "lokasi_alamat": this.laporan.alamat,
+      "nohp":"085745936410",
+		  "geolokasi": {
+			"lat": position.coords.latitude,
+			"lng": position.coords.longitude
+		  }
+		};
+		let headers = new Headers({ 'Content-Type': 'application/json' });
+		let options = new RequestOptions({ headers: headers });
+		this.http.post(url, postBodyLogin, options)
+				.subscribe(data =>
+				{
+					console.log(data);
+					this.showing = true;
+					this.laporan.jenis = 0;
+					this.laporan.alamat = "";
+					this.laporan.isiLaporan = "";
+					this.base64Image = "build/img/150x150.png";
+					let alert = this.alertController.create({
+					  title: 'Selamat',
+					  subTitle: 'Pengiriman laporan berhasil',
+					  buttons: ['OK']
+					});
+					alert.present();
+				},
+				error =>
+				{
+					this.showing = true;
+					console.log(error);
+					this.showAlert();
+				});
+	},
+	(error)=> {
+	    this.showing = true;
+		let alert = this.alertController.create({
+		  title: 'Perhatian',
+		  subTitle: 'Geolokasi (mendapatkan koordinat gagal, tidak bisa kirim data',
+		  buttons: ['OK']
+		});
+		alert.present();
+	});
   }
 
+    showAlert() {
+    let alert = this.alertController.create({
+      title: 'Perhatian',
+      subTitle: 'Pengiriman laporan Gagal, coba cek koneksi internet',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  captureCamera() {
+	Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 640,
+        targetHeight: 480
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+		console.log(this.base64Image);
+    }, (err) => {
+        console.log(err);
+    });
+  }
 }
